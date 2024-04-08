d9862b5ee63049d373f95c7d76ed8ae140210099
7RICK0 <beloaciolierick@gmail.com>
2024-04-08 18:09:42 -0300

    feito o html

--- gymconnect/webapp/templates/enviar_duvida.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('enviarDuvida').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    var conteudo = document.getElementById('conteudo').value; 
-    console.log(conteudo);
-    window.alert('Dúvida enviada com sucesso!');
-    document.getElementById('conteudo').value = "";
-});
