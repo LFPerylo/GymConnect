8850a58afe1e69faa354fbb2609831500ab2e4a9
luis perylo <lfpb@cesar.school>
2024-04-29 17:17:18 -0300

    .

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'hr2nvq',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
