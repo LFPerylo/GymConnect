fe5e8480562ad8e7ebbaf69513696ae40599ca46
luis perylo <lfpb@cesar.school>
2024-04-29 12:03:03 -0300

    selenium

--- gymconnect/webapp/tests.py
+++ gymconnect/webapp/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver= webdriver.Chrome()
+
+driver.get("https://www.google.com")
+pesquisa = driver
+
+time.sleep(5)
 
 # Create your tests here.
