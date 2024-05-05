be1d58fa6b6bbc0db58b0b533ae266e95d4b399f
SilvioFittipaldi <sfm@cesar.school>
2024-05-05 19:43:46 -0300

    .

--- gymconnect/urls.py
+++ gymconnect/urls.py
@@ -7,17 +7,14 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+
     path('', views.login, name="login"),
-    
     path('dicas/', views.dicas, name="dicas"),
-
     path('home_aluno/', views.home_aluno, name="homealuno"),
-
     path('home_adm/', views.home_adm, name="homeadm"),
-
     path('treinospredefinidos/', views.treinospredefinidos, name="treinospredefinidos"),
-
     path('duvidas/', views.duvidas, name="duvidas")
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
