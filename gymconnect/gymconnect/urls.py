12ac938467c3e39b55285c38d97a0aa543a39de3
rodrigobnm <150079017+rodrigobnm@users.noreply.github.com>
2024-05-06 11:32:15 -0300

    .

--- gymconnect/gymconnect/urls.py
+++ gymconnect/gymconnect/urls.py
@@ -1,20 +1,17 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from webapp import views  # Garanta que suas views estão corretamente importadas deste módulo
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-
     path('', views.login, name="login"),
     path('dicas/', views.dicas, name="dicas"),
     path('home_aluno/', views.home_aluno, name="homealuno"),
     path('home_adm/', views.home_adm, name="homeadm"),
     path('treinospredefinidos/', views.treinospredefinidos, name="treinospredefinidos"),
-    path('duvidas/', views.duvidas, name="duvidas")
-    
+    path('duvidas/', views.duvidas, name="duvidas"),
+    path('processar_formulario/', views.processar_formulario, name="processar_formulario"),  
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
